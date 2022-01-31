865098cc35072b0627282d1c768288258ea3e289
amanguptanalytics@gmail.com <amanguptanalytics@gmail.com>
2022-01-31 05:15:15 +0000

    commit

---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,16 @@
+with payments as (
+
+SELECT
+   id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    amount/100 as amount,
+    created as created_at,
+   
+
+    FROM `dbt-tutorial.stripe.payment`
+
+)
+
+Select * from payments
